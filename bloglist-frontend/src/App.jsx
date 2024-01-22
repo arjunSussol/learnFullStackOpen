084b8e4ac79bdb423f085f01e3fb990a1c60f12e
Arjun Sah <arjunsah@sussol.net>
2024-01-22 11:41:14 +0545

    create new blog with logged in user

--- bloglist-frontend/src/App.jsx
+++ bloglist-frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import { Login } from './components/Login'
+import { BlogForm } from './components/BlogForm'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -16,12 +17,28 @@ const App = () => {
     )  
   }, [])
 
+  useEffect(() => {
+    const loggedUser = window.localStorage.getItem('loggedBlogUser')
+    if (loggedUser) {
+      const user = JSON.parse(loggedUser)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const addNewBlog = async blogObject => {
+    const createdBlog = await blogService.create(blogObject)
+    setBlogs(blogs.concat(createdBlog))
+
+  }
+
   const handleLogin = async event => {
     event.preventDefault()
     try {
       const user = await loginService.login({ username, password })
       window.localStorage.setItem('loggedBlogUser', JSON.stringify(user))
       setUser(user)
+      blogService.setToken(user.token)
     } catch (error) {
       
     }  
@@ -53,6 +70,7 @@ const App = () => {
       <div>
         <h2>blogs</h2>
         <strong>{user.name} is logged in <button onClick={handleLogout}>Logout</button></strong>
+        <BlogForm createBlog={addNewBlog}/>
         {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
         )}
---
+++ bloglist-frontend/src/components/BlogForm.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react"
+
+export const BlogForm = ({ createBlog }) => {
+    const [title, setTitle] = useState('')
+    const [author, setAuthor] = useState('')
+    const [url, setUrl] = useState('')
+
+    const addBlog = (event) => {
+        event.preventDefault()
+        createBlog({
+            title,
+            author,
+            url
+        })
+        setTitle('')
+        setAuthor('')
+        setUrl('')
+    }
+
+    return(
+        <div>
+            <h2>Create new blog</h2>
+            <form onSubmit={addBlog}>
+                <div>
+                    <label htmlFor="title">Title</label>
+                    <input id="title" type="text" value={title} onChange={({ target }) => setTitle(target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="author">Author</label>
+                    <input id="author" type="text" value={author} onChange={({ target }) => setAuthor(target.value)} />
+                </div>
+                <div>
+                    <label htmlFor="url">URL</label>
+                    <input id="url" type="text" value={url} onChange={({ target }) => setUrl(target.value)} />
+                </div>
+                <button type="submit">Create</button>
+            </form>
+        </div>
+    )
+}
