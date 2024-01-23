9d702cfcec9094bca7de2c2eb4d667d8d42a906d
Arjun Sah <arjunsah@sussol.net>
2024-01-23 15:41:35 +0545

    show or hide blog by click event

--- bloglist-frontend/src/components/Blog.jsx
+++ bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,31 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const[visible, setVisibility] = useState(false)
+
+  const handleVisibility = () => {
+    setVisibility(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} <button onClick={handleVisibility}>{visible ? 'Hide' : 'Show'}</button>
+        {visible && <div>
+            <li>{blog.url}</li>
+            <li>{blog.likes} <button>like</button></li>
+            <li>{blog.author}</li>
+          </div>}
+      </div>
+  </div>
+)}
+
+const blogStyle = {
+  paddingTop: 10,
+  paddingLeft: 2,
+  border: 'solid',
+  borderWidth: 1,
+  marginBottom: 5
+}
 
 export default Blog
